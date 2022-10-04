4e7a546a703f9ff8cad2f5ea2e38aaf7a1cac922
Natalia Kozak <90988795+Natalja21@users.noreply.github.com>
2022-10-04 22:26:46 +0300

    app

---
+++ src/Servis/Api.jsx
@@ -0,0 +1,3 @@
+// const API_KEY = 'd7d9ab2863712e08ff43c2cdf6d8066a';
+// const URL = 'https://api.themoviedb.org/3/movie/550';
+// https://api.themoviedb.org/3/movie/550?api_key=d7d9ab2863712e08ff43c2cdf6d8066a
