6cf7a743bfe4a3e291dc026dd67d4c33770f8a10
Natalia Kozak <90988795+Natalja21@users.noreply.github.com>
2022-10-19 00:36:33 +0300

    not found

--- src/pages/NotFound.styled.js
+++ src/pages/NotFound.styled.js
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
 
 export const NotFoundTitle = styled.h2`
+margin-top: 60px;
 text-align:center;
 color:orange;
 `;
 export const NotFoundImg = styled.img`
 width: 300px;
 height:100%;
-margin: 10px auto;
+margin: 20px auto;
 border-radius: 50%;
 `;
 
 export const NotFoundText = styled.p`
 text-align:center;
+
 
 `
 
